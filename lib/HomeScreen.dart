f9b81441611908c8973d00fcdaf516b324fb9083
sahilyadav9149 <sahilyadav9149@gmail.com>
2024-10-20 23:26:40 +0530

    my first commit

---
+++ lib/HomeScreen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Homescreen extends StatelessWidget {
+  const Homescreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue[300],
+        title: Text('Chats'),
+      ),
+      body: const Center(
+        child: Text('Welcome to Second Barin '),
+      ),
+    );
+  }
+}
